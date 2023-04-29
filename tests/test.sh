5fb93ad7d7f1d91fba215b3fe8f6afd9e74303a3
Thyodas <guillaume.hein@epitech.eu>
2023-04-29 16:42:14 +0200

    feat: add readelf to test

--- tests/test.sh
+++ tests/test.sh
@@ -7,7 +7,9 @@ pid=$(pidof ./m32)
 cat "/proc/${pid}/maps"
 objdump -M intel -d ./m32 > m32.asm
 objdump -fs ./m32 > m32_objdump
+readelf -r ./m32 > m32_readelf
 cat m32.asm
 cat m32_objdump
+cat m32_readelf
 wait
 cat out
